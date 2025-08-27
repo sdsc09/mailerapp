36ea8b6d555036c68c82fdedd4264437eb23cd8f
Stefi <stefin09@hotmail.com>
2025-08-26 22:22:27 -0500

    .

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,7 @@ def create_app():
         DATABASE_HOST=os.environ.get('DATABASE_HOST'),
         DATABASE_USER=os.environ.get('DATABASE_USER'),
         DATABASE_PASSWORD=os.environ.get('DATABASE_PASSWORD'),
-        DATABAS=os.environ.get('DATABASE')
+        DATABASE=os.environ.get('DATABASE')
     )
 
     # Importa y registra el blueprint
